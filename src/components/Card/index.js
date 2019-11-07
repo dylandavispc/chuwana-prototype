55fc1798693118e4906270afbdad32843b814f1f
iuglyboi28 <iuglyboi28@gmail.com>
2019-11-07 10:06:12 -0500

    Dylan yo ass won't answer the phone, test this on your laptop when you can beautiful

---
+++ src/components/Card/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+function Card() {
+  return (
+    <div class="card">
+      <img src="..." class="card-img-top" alt="..." />
+      <div class="card-body">
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+  return (
+    <div>
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Chuana Mother Fuckaaaa</h1>
+          <p class="lead"></p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Jumbotron;
