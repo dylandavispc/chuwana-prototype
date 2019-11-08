70d0a31d2768e5fa7b2cf2c3bf7d44f263ca5191
Dylan Davis <dylandavispc@gmail.com>
2019-11-07 19:06:41 -0500

    Login working

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./components/pages/Home"
+import Landing from "./components/pages/Landing"
+import Register from "./components/pages/Register"
+import Login from "./components/pages/Login"
+import Profile from "./components/pages/Profile"
 
 function App() {
     return (
       <Router>
         <div>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/profile" component={Profile} />
         </div>
       </Router>
     );
---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+require("mongoose-type-email");
+bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: mongoose.SchemaTypes.Email },
+    password: {type: String, default: ""}
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
