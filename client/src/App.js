a67189c8d2c91543890420aaa4499210760e4ac7
iuglyboi28 <iuglyboi28@gmail.com>
2019-11-08 09:45:29 -0500

    Landing issues

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Landing from "./components/pages/Landing";
 import Register from "./components/pages/Register";
-import Login from "./components/pages/Login";
-import Profile from "./components/pages/Profile";
+// import Login from "./components/pages/Login";
+// import Profile from "./components/pages/Profile";
 
 function App() {
   return (
--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
-import Form from "../RegisterForm";
 
 class Landing extends Component {
   state = {
