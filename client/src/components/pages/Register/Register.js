cd7ca3e9c7a8980f13ff5ad32d2897b5233b1145
iuglyboi28 <iuglyboi28@gmail.com>
2019-11-14 13:48:51 -0500

    styling

--- client/src/components/pages/Register/Register.js
+++ client/src/components/pages/Register/Register.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 // import  register  from "./UserFunctions";
-
+import "./Register.css";
 import API from "../../../utils/API";
 
 class Register extends Component {
@@ -39,77 +39,70 @@ class Register extends Component {
 
   onSubmit = event => {
     event.preventDefault();
-    if (
-      this.state.email &&
-      this.state.password
-    ) {
+    if (this.state.email && this.state.password) {
       API.postUser({
         email: this.state.email,
         password: this.state.password
       })
         .then(response => {
-          console.log(response)
+          console.log(response);
           if (!response.data.errmsg) {
-            console.log('successful signup')
-            this.setState({ //redirect to login page
-              redirectTo: '/login'
-            })
+            console.log("successful signup");
+            this.setState({
+              //redirect to login page
+              redirectTo: "/login"
+            });
           } else {
-            console.log('username already taken')
+            console.log("username already taken");
           }
-        }).catch(error => {
-          console.log('signup error: ')
-          console.log(error)
-
         })
-      }
+        .catch(error => {
+          console.log("signup error: ");
+          console.log(error);
+        });
+    }
   };
 
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6 mt-5 mx-auto">
-            <form noValidate onSubmit={this.onSubmit}>
-              <h1 className="h3 mb-3 font-weight-normal">Register</h1>
-              <div className="form-group">
-                <label htmlFor="email">Email address</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  name="email"
-                  placeholder="Enter email"
-                  value={this.state.email}
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  name="password"
-                  placeholder="Password"
-                  value={this.state.password}
-                  onChange={this.onChange}
-                />
-              </div>
-              <button
-                type="submit"
-                className="btn btn-lg btn-primary btn-block"
-                onClick={this.onSubmit}
-                disabled={
-                  !(
-                    this.state.email &&
-                    this.state.password
-                  )
-                }
-              >
-                Register!
-              </button>
-            </form>
-          </div>
-        </div>
+      <div class="bg-img">
+        <form class="container">
+          <h1>Register</h1>
+
+          <label for="email">
+            <b>Email</b>
+          </label>
+          <input
+            placeholder="Enter Email"
+            name="email"
+            required
+            type="email"
+            className="form-control"
+            value={this.state.email}
+            onChange={this.onChange}
+          />
+
+          <label for="psw">
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.onChange}
+          />
+
+          <button
+            type="submit"
+            class="btn"
+            onClick={this.onSubmit}
+            disabled={!(this.state.email && this.state.password)}
+          >
+            Register
+          </button>
+        </form>
       </div>
     );
   }
